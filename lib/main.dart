dbc075b260dbed131211940cf5ebbb3e472cf83c
Frances Coleman <francescoleman@localhost.localdomain>
2020-08-08 12:25:20 +0100

    polylines

--- lib/main.dart
+++ lib/main.dart
@@ -52,106 +52,112 @@ class _MapState extends State<Map> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _initialPosition,
-            zoom: 15.0,
-          ),
-          myLocationEnabled: true,
-          mapType: MapType.normal,
-          compassEnabled: true,
-          markers: _markers,
-          onCameraMove: _onCameraMove,
-        ),
-
-        Positioned(
-          top: 50.0,
-          right: 15.0,
-          left: 15.0,
-          child: Container(
-            height: 50.0,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(3.0),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.blueGrey,
-                    offset: Offset(1.0, 5.0),
-                    blurRadius: 10,
-                    spreadRadius: 3)
-              ],
-            ),
-            child: TextField(
-              cursorColor: Colors.black,
-              controller: locationController,
-              decoration: InputDecoration(
-                icon: Container(
-                  margin: EdgeInsets.only(left: 20.0, top: 5.0),
-                  width: 10.0,
-                  height: 10,
-                  child: Icon(Icons.location_on, color: Colors.black),
+    return _initialPosition == null
+        ? Container(
+            alignment: Alignment.center,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ))
+        : Stack(
+            children: <Widget>[
+              GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: CameraPosition(
+                  target: _initialPosition,
+                  zoom: 15.0,
                 ),
-                hintText: "Enter Current Location!.....",
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
+                myLocationEnabled: true,
+                mapType: MapType.normal,
+                compassEnabled: true,
+                markers: _markers,
+                onCameraMove: _onCameraMove,
               ),
-            ),
-          ),
-        ),
 
-        Positioned(
-          top: 105.0,
-          right: 15.0,
-          left: 15.0,
-          child: Container(
-            height: 50.0,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(3.0),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.blueGrey,
-                    offset: Offset(1.0, 5.0),
-                    blurRadius: 10,
-                    spreadRadius: 3)
-              ],
-            ),
-            child: TextField(
-              cursorColor: Colors.black,
-              decoration: InputDecoration(
-                icon: Container(
-                    margin: EdgeInsets.only(left: 20.0, top: 5.0),
-                    width: 10.0,
-                    height: 10,
-                    child: Icon(Icons.local_taxi, color: Colors.yellow)),
-                hintText: "Destination......",
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
+              Positioned(
+                top: 50.0,
+                right: 15.0,
+                left: 15.0,
+                child: Container(
+                  height: 50.0,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(3.0),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.blueGrey,
+                          offset: Offset(1.0, 5.0),
+                          blurRadius: 10,
+                          spreadRadius: 3)
+                    ],
+                  ),
+                  child: TextField(
+                    cursorColor: Colors.black,
+                    controller: locationController,
+                    decoration: InputDecoration(
+                      icon: Container(
+                        margin: EdgeInsets.only(left: 20.0, top: 5.0),
+                        width: 10.0,
+                        height: 10,
+                        child: Icon(Icons.location_on, color: Colors.black),
+                      ),
+                      hintText: "Enter Current Location!.....",
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ),
-        ),
 
-        // Positioned(
-        //   top: 40,
-        //   right: 10,
-        //   child: FloatingActionButton(
-        //     onPressed: _onAddMarkerPressed,
-        //     tooltip: "Addmarker",
-        //     backgroundColor: black,
-        //     child: Icon(
-        //       Icons.add_location,
-        //       color: white,
-        //     ),
-        //   ),
-        // ),
-      ],
-    );
+              Positioned(
+                top: 105.0,
+                right: 15.0,
+                left: 15.0,
+                child: Container(
+                  height: 50.0,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(3.0),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.blueGrey,
+                          offset: Offset(1.0, 5.0),
+                          blurRadius: 10,
+                          spreadRadius: 3)
+                    ],
+                  ),
+                  child: TextField(
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                      icon: Container(
+                          margin: EdgeInsets.only(left: 20.0, top: 5.0),
+                          width: 10.0,
+                          height: 10,
+                          child: Icon(Icons.local_taxi, color: Colors.yellow)),
+                      hintText: "Destination......",
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
+                    ),
+                  ),
+                ),
+              ),
+
+              // Positioned(
+              //   top: 40,
+              //   right: 10,
+              //   child: FloatingActionButton(
+              //     onPressed: _onAddMarkerPressed,
+              //     tooltip: "Addmarker",
+              //     backgroundColor: black,
+              //     child: Icon(
+              //       Icons.add_location,
+              //       color: white,
+              //     ),
+              //   ),
+              // ),
+            ],
+          );
   }
 
   void _onMapCreated(GoogleMapController controller) {
@@ -166,7 +172,7 @@ class _MapState extends State<Map> {
     });
   }
 
-  void _onAddMarkerPressed() {
+  void _onaddMarker() {
     setState(() {
       _markers.add(Marker(
           markerId: MarkerId(lastposition.toString()),
@@ -174,6 +180,17 @@ class _MapState extends State<Map> {
           infoWindow: InfoWindow(title: "Coleman!", snippet: "Designs"),
           icon: BitmapDescriptor.defaultMarker));
     });
+  }
+
+  List<LatLng> convertTolatLng(List points) {
+    List<LatLng> result = <LatLng>[];
+    for (int i = 0; i < points.length; i++) {
+      if (i % 2 != 0) {
+        result.add(LatLng(points[i - 1], points[i]));
+      }
+    }
+
+    return result;
   }
 
   // !DECODE POLY
@@ -220,5 +237,16 @@ class _MapState extends State<Map> {
       _initialPosition = LatLng(position.latitude, position.longitude);
       locationController.text = placemark[0].name;
     });
+  }
+
+  void sendRequest(String intendedLocation) async {
+    List<Placemark> placemark =
+        await Geolocator().placemarkFromAddress(intendedLocation);
+    double latitude = placemark[0].position.latitude;
+    double longitude = placemark[0].position.longitude;
+    LatLng destination = LatLng(latitude, longitude);
+    _onaddMarker(destination, _initialPosition);
+    String route = await _googleMapsServices.getRouteCoordinates(
+        _initialPosition, destination);
   }
 }
